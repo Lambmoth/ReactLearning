30167d01980f3a875159a13812085a5d733f0b03
gjlandrews <gja@ualberta.ca>
2020-09-25 00:16:58 -0600

    added like buttons

--- src/App.js
+++ src/App.js
@@ -4,22 +4,33 @@ import Tweet from './tweet';
 
 function App(){
 
-  const [isRed, setRed] = useState(false);
-  const [count, setCount] = useState(0);
+  // const [isBlue, setBlue] = useState(false);
+  // const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+  // const increment = () => {
+  //   setCount(count + 1);
+  //   setBlue(!isBlue);
+  // };
+
+  const [users, setUsers] = useState([
+    { name: "Dalinar", message: "Follow the codes"},
+    { name: "Adolin", message: "Duel me irl"},
+    { name: "Rock", message: "Airsick lowlanders..."},
+    { name: "Kaladin", message: ":/"}
+  ]);
 
   return(
     <div className="app">
-      <h1>Change my color!</h1>
+      {/* <h1 className={isBlue ? "blue" : ""}>Change my color!</h1>
       <button onClick={increment}>Increment</button>
-      <h1>{count}</h1>
+      <h1>{count}</h1> */}
       {/* <Tweet name="Dalinar" message="Follow the codes"/>
       <Tweet name="Adolin" message="Duel me irl"/>
       <Tweet name="Rock" message="Airsick lowlanders..."/>
       <Tweet name="Kaladin" message=":("/> */}
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
 }
--- src/tweet.js
+++ src/tweet.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./App.css";
 
 function Tweet({ name, message }) {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count+1)
+    };
+
+    const decrement = () => {
+        setCount(count-1)
+    };
     return(
         <div className="tweet">
             <h3>{name}</h3>
             <p>{message}</p>
             <h3>Number of Likes</h3>
+            <div>
+                <button onClick={decrement}>-1</button>
+                <h3>{count}</h3>
+                <button onClick={increment}>+1</button>
+            </div>
         </div>
     );
 }
